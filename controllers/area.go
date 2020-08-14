e657de266387b28834eacef140280c543ee86bf1
origin <jsorigin@itcom888.com>
2020-08-15 02:41:03 +0800

    feat: getArea方法

--- controllers/area.go
+++ controllers/area.go
@@ -1,39 +1,51 @@
 package controllers
 
 import (
-	"encoding/json"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"loveHome2020/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type AreaController struct {
 	beego.Controller
 }
 
+func (this *AreaController) RetData(resp map[string]interface{}) {
+	this.Data["json"] = &resp
+	this.ServeJSON()
+}
+
 func (c *AreaController) GetArea() {
 	beego.Info("GetArea sucusse")
 
-	resp := map[string]interface{}
+	var resp map[string]interface{}
+	defer c.RetData(resp)
 
 	// 从数据库中读取数据
-	area := models.Area{}
+	areasModel := []models.Area{}
 
 	o := orm.NewOrm()
-	err := o.Read(&area)
-	if err != nil {
+	_, allErr := o.QueryTable("area").All(&areasModel)
+	if allErr != nil {
 		beego.Info("数据错误")
 		resp["errno"] = 4001
 		resp["errmsg"] = "查询失败"
 
 		return
 	}
+	if allErr != nil {
+		resp["errno"] = 4002
+		resp["errmsg"] = "没有查到数据"
+		return
+	}
 	resp["errno"] = 0
 	resp["errmsg"] = "OK"
-	resp["data"] = &area
+	resp["data"] = areasModel
 
-	json_str := json.Marshal(resp)
-	c.Ctx.WriteString(json_str)
-	c.Data["json"] = resp
-	c.ServeJSON()
+	beego.Info("数据查询成功")
+	//json_str := json.Marshal(resp)
+	//c.Ctx.WriteString(json_str)
+	//c.Data["json"] = resp
+	//c.ServeJSON()
 }
