2606129f44ed5c53db7972c5d3b9abadef59b7a1
origin <jsorigin@itcom888.com>
2020-08-17 03:14:02 +0800

    feat: 注册方法

--- controllers/users.go
+++ controllers/users.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"loveHome2020/models"
 )
 
 type UserController struct {
@@ -23,6 +25,21 @@ func (this *UserController) Reg() {
 	beego.Info(`resp["mobile"] =`, resp["mobile"])
 	beego.Info(`resp["password"] =`, resp["password"])
 	beego.Info(`resp["sms_code"] =`, resp["sms_code"])
+
+	//插入数据库
+	o := orm.NewOrm()
+	user := models.User{}
+	user.Password_hash = resp["password"].(string)
+	user.Name = resp["mobile"].(string)
+	user.Mobile = resp["mobile"].(string)
+	id, err := o.Insert(&user)
+	if err != nil {
+		resp["errno"] = 4002
+		resp["errmsg"] = "注册失败"
+	}
+	beego.Info("reg success, id =", id)
+	resp["errno"] = 0
+	resp["errmsg"] = "注册成功"
 
 	this.RetData(resp)
 }
