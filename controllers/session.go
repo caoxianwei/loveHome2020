80bba05b0425efdd0d9fae912c3a0c261c7c9558
origin <jsorigin@itcom888.com>
2020-08-17 14:39:51 +0800

    ”updagitignore“

--- controllers/session.go
+++ controllers/session.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"loveHome2020/models"
+)
 
 type SessionController struct {
 	beego.Controller
@@ -15,7 +18,12 @@ func (this *SessionController) SessionData() {
 	beego.Info("GetHouseIndex function")
 
 	resp := make(map[string]interface{})
-	resp["errno"] = 4001
-	resp["errmsg"] = "查询失败"
+
+	user :=models.User{}
+	user.Name = "eeeewww1"
+
+	resp["errno"] = 0
+	resp["errmsg"] = "OK"
+	resp["data"] = user
 	this.RetData(resp)
 }
