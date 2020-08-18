c314426c7ffa1c82a8187c930c2463731136fc80
origin <jsorigin@itcom888.com>
2020-08-19 03:12:43 +0800

    feat: 连接redis

--- controllers/area.go
+++ controllers/area.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/astaxie/beego/cache/redis"
 	"loveHome2020/models"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 type AreaController struct {
@@ -19,12 +23,27 @@ func (this *AreaController) RetData(resp map[string]interface{}) {
 func (this *AreaController) GetArea() {
 	beego.Info("GetArea sucusse")
 
-	var resp map[string]interface{}
+	//var resp map[string]interface{}
+	resp := make(map[string]interface{})
 	defer this.RetData(resp)
 
+	// 从Redis中读取数据
+	// 初始化一个全局变量对象
+	cache_conn, err := cache.NewCache("redis", `{"key":"lovehome","conn":":6379","dbNum":"0"}`)
+	if err != nil {
+		resp["errno"] = models.RECODE_DBERR
+		resp["errmsg"] = models.RecodeText(models.RECODE_DBERR)
+		return
+	}
+
+	errCache := cache_conn.Put("aaa", "bbbbaaa", time.Second * 3600)
+	if errCache != nil {
+		beego.Error("cacha err")
+	}
+	beego.Info("cache_conn.aa = ", cache_conn.Get("aaa"))
+	fmt.Printf("cache_conn[aa] = %s\n ",cache_conn.Get("aaa"))
 	// 从数据库中读取数据
 	areasModel := []models.Area{}
-
 	o := orm.NewOrm()
 	num, allErr := o.QueryTable("area").All(&areasModel)
 	if allErr != nil {
